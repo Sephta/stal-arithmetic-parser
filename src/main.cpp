2d73984045def026a24618c5cf267820892fdfde
Seth Tal <Seth.tal255@gmail.com>
2021-07-29 18:40:20 -0700

    Adding a couple temporary tests

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,16 @@ void AtomicValue::setValue(int newValue) { this->value = newValue; };
 #pragma endregion
 
 
-int main()
+int main(int argc, char** argv)
 {
+  using namespace std;
+
+  cout << "ARGC: " << argc << endl;
+  for (int i = 0; i < argc; i++)
+    cout << "ARGV[" << i << "]" << " = " << argv[i] << endl;
+  
+  cout << endl;
+
   AtomicValue* av_test = new AtomicValue(42);
 
   Operator newOp = Operator::Add;
